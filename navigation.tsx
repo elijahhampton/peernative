2bf0695b2c2fcc308e467912acc90acfdb783ea4
king-midas <mto0006@auburn.edu>
2023-04-26 18:26:01 -0500

    Update BottomNav so that icons are included. Original code is commented in case its needed since the current edit is scratched.

--- navigation.tsx
+++ navigation.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 import Main from './components/Main';
 import Settings from './pages/Settings';
@@ -10,7 +11,29 @@ const Stack = createBottomTabNavigator();
 
 function Navigation(): JSX.Element {
   return (
-    <Stack.Navigator screenOptions={{headerShown: false}}>
+    <Stack.Navigator
+      screenOptions={({route}) => ({
+        headerShown: false,
+        tabBarIcon: ({focused, color, size}) => {
+          let iconName;
+
+          if (route.name === 'Home') {
+            iconName = focused ? 'home' : 'home-outline';
+          } else if (route.name === 'Settings') {
+            iconName = focused ? 'settings' : 'settings-outline';
+          } else if (route.name === 'About') {
+            iconName = focused
+              ? 'information-circle'
+              : 'information-circle-outline';
+          }
+
+          return <Icon name={iconName} size={size} color={color} />;
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: 'tomato',
+        inactiveTintColor: 'gray',
+      }}>
       <Stack.Screen name="Welcome" component={Welcome} />
       <Stack.Screen name="Home" component={Main} />
       <Stack.Screen name="Settings" component={Settings} />
@@ -20,3 +43,26 @@ function Navigation(): JSX.Element {
 }
 
 export default Navigation;
+
+// import React from 'react';
+// import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+// import Main from './components/Main';
+// import Settings from './pages/Settings';
+// import Welcome from './pages/Welcome';
+// import About from './pages/About';
+
+// const Stack = createBottomTabNavigator();
+
+// function Navigation(): JSX.Element {
+//   return (
+//     <Stack.Navigator screenOptions={{headerShown: false}}>
+//       <Stack.Screen name="Welcome" component={Welcome} />
+//       <Stack.Screen name="Home" component={Main} />
+//       <Stack.Screen name="Settings" component={Settings} />
+//       <Stack.Screen name="About" component={About} />
+//     </Stack.Navigator>
+//   );
+// }
+
+// export default Navigation;
