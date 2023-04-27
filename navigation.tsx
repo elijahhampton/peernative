73a7008f2ba6b4ed88d132f11af3e8aa712b6d1f
king-midas <mto0006@auburn.edu>
2023-04-27 18:06:49 -0500

    Update icons

--- navigation.tsx
+++ navigation.tsx
@@ -1,20 +1,58 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 import Main from './components/Main';
 import Settings from './pages/Settings';
 import Welcome from './pages/Welcome';
 import About from './pages/About';
 
+interface IconProps {
+  color: string;
+  size: number;
+}
+
 const Stack = createBottomTabNavigator();
+
+const HomeIcon = ({color, size}: IconProps) => (
+  <Icon name="ios-home-outline" color={color} size={size} />
+);
+
+const ChatIcon = ({color, size}: IconProps) => (
+  <Icon name="ios-chatbubbles-outline" color={color} size={size} />
+);
+
+const SettingsIcon = ({color, size}: IconProps) => (
+  <Icon name="ios-settings-outline" color={color} size={size} />
+);
+
+const AboutIcon = ({color, size}: IconProps) => (
+  <Icon name="ios-information-circle-outline" color={color} size={size} />
+);
 
 function Navigation(): JSX.Element {
   return (
     <Stack.Navigator screenOptions={{headerShown: false}}>
-      <Stack.Screen name="Welcome" component={Welcome} />
-      <Stack.Screen name="Home" component={Main} />
-      <Stack.Screen name="Settings" component={Settings} />
-      <Stack.Screen name="About" component={About} />
+      <Stack.Screen
+        name="Welcome"
+        component={Welcome}
+        options={{tabBarIcon: HomeIcon}}
+      />
+      <Stack.Screen
+        name="Chat"
+        component={Main}
+        options={{tabBarIcon: ChatIcon}}
+      />
+      <Stack.Screen
+        name="Settings"
+        component={Settings}
+        options={{tabBarIcon: SettingsIcon}}
+      />
+      <Stack.Screen
+        name="About"
+        component={About}
+        options={{tabBarIcon: AboutIcon}}
+      />
     </Stack.Navigator>
   );
 }
@@ -23,7 +61,6 @@ export default Navigation;
 
 // import React from 'react';
 // import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-// import Icon from 'react-native-vector-icons/Ionicons';
 
 // import Main from './components/Main';
 // import Settings from './pages/Settings';
@@ -34,29 +71,7 @@ export default Navigation;
 
 // function Navigation(): JSX.Element {
 //   return (
-//     <Stack.Navigator
-//       screenOptions={({route}) => ({
-//         headerShown: false,
-//         tabBarIcon: ({focused, color, size}) => {
-//           let iconName;
-
-//           if (route.name === 'Chat') {
-//             iconName = focused ? 'chatbox' : 'chatbox-ellipses-outline';
-//           } else if (route.name === 'Settings') {
-//             iconName = focused ? 'settings' : 'settings-outline';
-//           } else if (route.name === 'About') {
-//             iconName = focused
-//               ? 'information-circle'
-//               : 'information-circle-outline';
-//           }
-
-//           return <Icon name={iconName} size={size} color={color} />;
-//         },
-//       })}
-//       tabBarOptions={{
-//         activeTintColor: 'tomato',
-//         inactiveTintColor: 'gray',
-//       }}>
+//     <Stack.Navigator screenOptions={{headerShown: false}}>
 //       <Stack.Screen name="Welcome" component={Welcome} />
 //       <Stack.Screen name="Chat" component={Main} />
 //       <Stack.Screen name="Settings" component={Settings} />
